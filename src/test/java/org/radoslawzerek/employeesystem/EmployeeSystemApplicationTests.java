3568b3d4fda3300f6d5f9de184e40852acb93536
Radoslaw <zerek.r93@gmail.com>
2022-11-10 13:44:32 +0100

    Initial commit

---
+++ src/test/java/org/radoslawzerek/employeesystem/EmployeeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.radoslawzerek.employeesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
